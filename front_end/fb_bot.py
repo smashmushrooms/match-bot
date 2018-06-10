55c9210491b2192911de2995f4eaf75205b73c92
Nikita Korobov <korobov@visilletc.com>
2018-06-10 21:06:30 +0300

    ADD: front_end: In pursuit to previous commit

--- front_end/fb_bot.py
+++ front_end/fb_bot.py
@@ -2,10 +2,11 @@
 import random
 from flask import Flask, request
 from pymessenger.bot import Bot
+from pymessenger import Element, Button
 
 app = Flask(__name__)
-ACCESS_TOKEN = 'ACCESS_TOKEN'
-VERIFY_TOKEN = 'VERIFY_TOKEN'
+ACCESS_TOKEN = 'EAAEtr6bH9LEBAKXpBq732AhmrdwLV3EJynZCYFLnqRahVqOHEtZCWjD3IoKdOvLepZAmZAcPKlpEBlM16WB6WTroZCRkZAadHHlX7tcYdApMZBLg8YQAQyp0JXKEJ031NG0ud5ztpAZAL1Dy6ZAAn3Rb6l80jMJEyiUbZC6PqrdZAGTLRmgZCMOh4NSLfV1FzEw7GDAZD'
+VERIFY_TOKEN = 'ourbadpass123'
 bot = Bot(ACCESS_TOKEN)
 
 #We will receive messages that Facebook sends our bot at this endpoint 
@@ -35,26 +36,30 @@ def receive_message():
                     send_message(recipient_id, response_sent_nontext)
     return "Message Processed"
 
-
-def verify_fb_token(token_sent):
-    #take token sent by facebook and verify it matches the verify token you sent
-    #if they match, allow the request, else return an error 
+def verify_fb_token(token_sent): 
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     return 'Invalid verification token'
 
-
-#chooses a random message to send to the user
 def get_message():
     sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-    # return selected item to the user
     return random.choice(sample_responses)
 
-#uses PyMessenger to send response to user
 def send_message(recipient_id, response):
-    #sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
     return "success"
+
+def send_buttons(recipient_id, inbuttons, action_description):
+    buttons = []
+    for inbtn in inbuttons:
+        button = Button(title=inbtn[0], type=inbtn[1], payload='other')
+        buttons.append(button)
+    bot.send_button_message(recipient_id, action_description, buttons)
+    return "success"
+
+def send_photo(recipient_id, photo_path):
+    result = bot.send_image(recipient_id, photo_path)
+    return result
 
 if __name__ == "__main__":
     app.run()
