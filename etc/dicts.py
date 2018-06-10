7d5e8b6e0a79875df1b3a7394079353b28068b3c
Nikita Korobov <korobov@visilletc.com>
2018-06-10 20:42:22 +0300

    ADD: front_end: Updated FB API

---
+++ etc/dicts.py
@@ -0,0 +1,4 @@
+teams = {   'Germany': 1,
+            'Russia': 2,
+            'Spain' : 3
+        }
---
+++ user/user.py
@@ -0,0 +1,6 @@
+class User():
+    __name = ''
+    __lovely_team = -1
+
+    def __init__():
+        pass
